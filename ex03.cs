c485460d3c21425f60519fd1a61770229463ba2e
Corentin M <corentin.metz-dubois@epitech.eu>
2024-04-16 11:47:11 +0200

    ex03

---
+++ ex03.cs
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** Workshop-Csharp
+** File description:
+** ex03
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create list
+        // Read user input
+        // Store input in list
+        // Print list
+    }
+}
